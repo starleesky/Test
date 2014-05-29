8b874aca0619adc6348ed3e2ceef9d52855a6694
starleesky <starleesky@126.com>
2014-05-29 14:38:21 +0800

    Test brach

---
+++ src/com/Test.java
@@ -0,0 +1,18 @@
+/**
+ * This file is part of 139erp product.
+ * Copyright: Hangzhou Shenzhu Co. Ltd., All rights reserved
+ */
+package com;
+
+/**
+ * <p>
+ * Test.java
+ * </p>
+ * 
+ * Created: 2014年5月29日 下午2:37:23
+ *
+ * @author lixin(lixin@139erp.com)
+ */
+public class Test {
+
+}
