426291a014ed1e3c20e9e193508dfb76c04ccf0e
starleesky <starleesky@126.com>
2014-05-29 15:11:02 +0800

    add

---
+++ src/HelloWorld.java
@@ -0,0 +1,25 @@
+/**
+ * This file is part of 139erp product.
+ * Copyright: Hangzhou Shenzhu Co. Ltd., All rights reserved
+ */
+
+/**
+ * <p>
+ * HelloWorld.java
+ * </p>
+ * 
+ * Created: 2014年5月29日 下午2:03:10
+ * 
+ * @author lixin(lixin@139erp.com)
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("hi~!!! Hellow World");
+		
+		System.out.println("oye!!!");
+		//这不是测试
+		System.out.println("33");
+	}
+
+}
