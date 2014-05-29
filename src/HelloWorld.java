ac8d0b366fa01fcf160e85901695dcecdc952023
starleesky <starleesky@126.com>
2014-05-29 14:04:31 +0800

    测试数据

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * This file is part of 139erp product.
+ * Copyright: Hangzhou Shenzhu Co. Ltd., All rights reserved
+ */
+
+/**
+ * <p>
+ * HelloWorld.java
+ * </p>
+ * 
+ * Created: 2014年5月29日 下午2:03:10
+ *
+ * @author lixin(lixin@139erp.com)
+ */
+public class HelloWorld {
+
+}
